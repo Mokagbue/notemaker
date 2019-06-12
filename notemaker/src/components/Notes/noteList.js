93940297dd6f52830d0251576c07b8393ca72d8b
Gothic Jellybean <gothicjellybean@Gothics-iMac.local>
2019-06-11 23:06:30 -0600

    login working

--- notemaker/src/components/Notes/noteList.js
+++ notemaker/src/components/Notes/noteList.js
@@ -1,29 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { NavLink, Link } from 'react-router-dom';
+import axios from 'axios';
 
-
-function NotesList(props) {
-    return (
-        <div className="list-wrap">
-        <div className="navigation-box">
-            <nav className="nav">
-                <NavLink exact to="/" className="navigation-buttons">Home</NavLink>
-                <NavLink  to="/newNotes" className="navigation-buttons">New</NavLink>
-            </nav>
-            </div>
-            <h1 className="home-title">NOTES</h1>
-            <div className="page-box">
-                <div className="note-wrap">
-                    {props.notes.map((note, index) => (
-                        <div key={index} className="note-card">
-                            <Link to={`/notes/${note.id}`}>
-                                <h3 className="note-title">{note.notes_title}</h3>
-                            </Link>
-                        </div>
-                    ))}
+class NotesList extends Component {
+    constructor() {
+        super();
+        this.state = {
+            notes: [],
+        }
+    };
+    componentDidMount() {
+        this.grabAllNotes();
+      }
+    
+      grabAllNotes = () => {
+        console.log("getting notes?", this.state);
+        axios.get('http://localhost:9000/api/notes/:id/users')
+        .then(response => this.setState({ ...this.state, notes: response.data }))
+        .catch(error => console.log(error));
+      }
+      render() {
+        return (
+            <div className="list-wrap">
+            <div className="navigation-box">
+                <nav className="nav">
+                    <NavLink exact to="/" className="navigation-buttons">Home</NavLink>
+                    <NavLink  to="/newNotes" className="navigation-buttons">New</NavLink>
+                </nav>
+                </div>
+                <h1 className="home-title">NOTES</h1>
+                <div className="page-box">
+                    <div className="note-wrap">
+                        {this.notes.map((note, index) => (
+                            <div key={index} className="note-card">
+                                <Link to={`/notes/${note.id}`}>
+                                    <h3 className="note-title">{note.notes_title}</h3>
+                                </Link>
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        );
+    }
 }
 export default NotesList;
