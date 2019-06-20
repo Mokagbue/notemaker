af2071a0455b4d38628890f2ce58b69aa490e53c
Gothic Jellybean <gothicjellybean@Gothics-iMac.local>
2019-06-19 20:44:28 -0600

    new notes db fix

---
+++ back-end/database/migrations/20190619203659_notesfour_table.js
@@ -0,0 +1,12 @@
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('notesfour', function(notes) {
+        notes.increments();
+        notes.string('notes_title').notNullable();
+        notes.text('notes_content').notNullable();
+        notes.string('username', 128).notNullable();
+    });
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('notesfour');
+  };
