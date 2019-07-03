cc67f71068092fec6834b330b96ba48dd56c22fd
Gothic Jellybean <gothicjellybean@Gothics-iMac.local>
2019-07-02 20:16:21 -0600

    note list styling

--- notemaker/src/Home/LandingPage.js
+++ notemaker/src/Home/LandingPage.js
@@ -26,48 +26,10 @@ class LandingPage extends Component {
               {/* <img src={Title} alt="title" className="title" /> */}
             </div>
             <div>
-              <h4 className="text">Leave a Note on the Wall</h4>
+              {/* <h4 className="text">Leave a Note on the Wall</h4> */}
             </div>     
           </div>         
         )
       }
     }
 export default LandingPage;
-
-
-// import React, { Component } from 'react';
-// // import auth from "../Auth/auth";
-
-// class Home extends Component {
-//     login() {
-//       this.props.auth.login();
-//     }
-//     render() {
-//       const { isAuthenticated } = this.props.auth;
-//       return (
-//         <div className="container">
-//           {
-//             isAuthenticated() && (
-//                 <h4>
-//                   You are logged in!
-//                 </h4>
-//               )
-//           }
-//           {
-//             !isAuthenticated() && (
-//                 <h4>
-//                   You are not logged in! Please{' '}
-//                   <a href="http:localhost:9000/api/notes" style={{ cursor: 'pointer' }}
-//                     onClick={this.login.bind(this)}>
-//                     Log In
-//                   </a>
-//                   {' '}to continue.
-//                 </h4>
-//               )
-//           }
-//         </div>
-//       );
-//     }
-//   }
-  
-//   export default Home;
